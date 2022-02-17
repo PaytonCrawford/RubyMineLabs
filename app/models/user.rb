afd7129d037d7456a7410f92a2fdf6d62180f57f
PaytonCrawford <W0742398@myscc.ca>
2022-02-17 17:39:10 -0500

    Updated validation presence: none for a few attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
+  validates :street_address, presence: none
+  validates :city, presence: none
+  validates :province, presence: none
+  validates :postal_code, presence: none
+  validates :country, presence: none
 end
